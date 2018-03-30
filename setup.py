a2507c9feb498317a7a2895dbd0bc8c05d571243
Christopher Bradley <Chris.BRADLEY@cy.id.au>
2018-03-30 22:19:03 +1100

    Clean up code, add CLI for listing, adding, and deleting domains

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
-from setuptools import setup
-
-
-def read(filename):
-    with open(filename, 'r') as f:
-        return f.read()
+from setuptools import setup, find_packages
 
 
 setup(
     name='ventraip-vip-client',
     description='Client to connect to and manage DNS entries registered with VentraIP',
-    long_description=read('README.rst'),
-    version='0.1.1',
+    version='0.2.0',
     url='https://github.com/cmbrad/ventraip-vip-client',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     author='Christopher Bradley',
     author_email='chris.bradley@cy.id.au',
     license='MIT',
@@ -20,6 +16,15 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers'
     ],
-    packages=['ventraip'],
-    install_requires=['requests>=2.18,<2.19', 'beautifulsoup4>=4.6,<4.7']
+    install_requires=[
+        'requests>=2.18,<2.19',
+        'beautifulsoup4>=4.6,<4.7',
+        'click>=6.7,<6.8',
+        'tabulate>=0.8,<0.9'
+    ],
+    entry_points={
+        'console_scripts': [
+            'ventraip = ventraip.cli:main',
+        ],
+    }
 )
--- src/ventraip/client.py
+++ src/ventraip/client.py
@@ -1,12 +1,57 @@
 import re
 import logging
+from typing import List, Optional
+
 import requests
 
 from bs4 import BeautifulSoup
 
-from ventraip.dns_record import DnsRecord
-from ventraip.domain import Domain
-from ventraip.exceptions import DnsRecordCreateFailedError, DnsRecordRemoveFailedError
+
+class Domain(object):
+    def __init__(self, internal_id, name):
+        self.internal_id = internal_id
+        self.name = name
+
+    def __repr__(self):
+        return f'{self.__class__.__name__} ({self.__dict__})'
+
+
+class DnsRecord(object):
+    def __init__(self, internal_id, hostname, destination, record_type, ttl, domain: Domain):
+        self.internal_id = internal_id
+        self.hostname = hostname
+        self.destination = destination
+        self.record_type = record_type
+        self.ttl = ttl
+        self.domain = domain
+
+    def fqdn(self):
+        return f'{self.hostname}.{self.domain.name}'
+
+    def __repr__(self):
+        return f'{self.__class__.__name__} ({self.__dict__})'
+
+
+class VipClientError(Exception):
+    def __repr__(self):
+        return f'{self.__class__.__name__} ({self.__dict__})'
+
+
+class DnsRecordActionError(VipClientError):
+    def __init__(self, message, url, params):
+        self.message = message
+        self.url = url
+        self.params = params
+
+        super().__init__(message)
+
+
+class DnsRecordCreateFailedError(DnsRecordActionError):
+    pass
+
+
+class DnsRecordRemoveFailedError(DnsRecordActionError):
+    pass
 
 
 class VipClient(object):
@@ -25,7 +70,7 @@ class VipClient(object):
         self._session = requests.Session()
 
     def login(self, email, password):
-        res = self._session.post('https://vip.ventraip.com.au', data={
+        self._session.post('https://vip.ventraip.com.au', data={
             'email': email,
             'password': password,
             'request': '0123456789'  # set in the original request, but providng dummy data seems to work
@@ -35,7 +80,7 @@ class VipClient(object):
 
         return self
 
-    def domains(self):
+    def domains(self) -> List[Domain]:
         res = self._session.get('https://vip.ventraip.com.au/home/domain/')
         soup = BeautifulSoup(res.content, 'html.parser')
         table = soup.find(id='domtbl')
@@ -52,30 +97,57 @@ class VipClient(object):
             # the expiry date is fetched later via ajax
             cols_text = [ele.text.strip() for ele in row.find_all('td')]
             # Domain name is in the 2nd cell of thr row
-            domains.append(Domain(hostname=cols_text[1], internal_id=domain_id))
+            domains.append(Domain(name=cols_text[1], internal_id=domain_id))
 
         return domains
 
-    def dns_records(self, domain_id):
+    def domain(self, domain_name) -> Optional[Domain]:
+        domains = self.domains()
+        for domain in domains:
+            if domain.name == domain_name:
+                return domain
+
+        return None
+
+    def dns_record(self, hostname, domain_name, record_type):
+        domains = self.domains()
+        for domain in domains:
+            if domain.name == domain_name:
+                dns_records = self.dns_records(domain.internal_id)
+
+                for dns_record in dns_records:
+                    if dns_record.hostname == hostname and record_type == record_type:
+                        return dns_record
+
+        return None
+
+    def dns_records(self, domain_id) -> List[DnsRecord]:
+        def _clean(s):
+            s = re.sub(r'[\t\n]+', '', s)
+            s = re.sub(r'[ ]+', ' ', s)
+            return s.strip()
+
         res = self._session.get(f'https://vip.ventraip.com.au/home/domain/{domain_id}/service')
         soup = BeautifulSoup(res.content, 'html.parser')
         table = soup.find('p', attrs={'class': 'title'}, text='DNS Records').findNext('table')
         table_body = table.find('tbody')
         rows = table_body.find_all('tr')
 
+        domain_name = [ele.text for ele in soup.find_all('h1') if 'Domain Name Service -' in ele.text][0]
+        domain_name = domain_name.replace('Domain Name Service - ', '')
+        domain = Domain(internal_id=domain_id, name=domain_name)
+
+
         records = []
         for row in rows:
             record_id = row.find('form').find('input').get('value')
-            cols_text = [self._clean(ele.text) for ele in row.find_all('td')]
+            cols_text = [_clean(ele.text) for ele in row.find_all('td')]
 
-            records.append(DnsRecord(internal_id=record_id, hostname=cols_text[0], destination=cols_text[1], record_type=cols_text[2], ttl=cols_text[3]))
+            records.append(DnsRecord(internal_id=record_id, hostname=cols_text[0].replace(domain_name, '').rstrip('.'),
+                                     destination=cols_text[1], record_type=cols_text[2], ttl=cols_text[3],
+                                     domain=domain))
 
         return records
-
-    def _clean(self, s):
-        s = re.sub(r'[\t\n]+', '', s)
-        s = re.sub(r'[ ]+', ' ', s)
-        return s.strip()
 
     def remove_dns_record(self, domain_id, dns_record_id):
         url = f'https://vip.ventraip.com.au/home/domain/{domain_id}/service'
@@ -101,7 +173,7 @@ class VipClient(object):
         data = {
             'dnshostname': hostname,
             'dnsdest': destination,
-            'dnsttl': 3600,
+            'dnsttl': ttl,
             self._dns_record_add_key[record_type]: 'Add'
         }
         res = self._session.post(url=url, data=data)
